a94958ad1b7300eb49cbde9895010d4ab71b8037
Sreedhar K <sreedhar.k@rakuten.com>
2024-09-30 15:49:40 +0530

    C

--- 50A_py.js
+++ 50A_py.js
@@ -1,3 +1,5 @@
+    /********************************************* Fixed code /*********************************************/
+
 let [fs , path,common] = [require('fs') , require('path'),require('./fixed_code')];
 
 console.log(`================================ ${path.parse(path.basename(__filename))["name"]} ================================`)
@@ -5,15 +7,17 @@ console.log(`================================ ${path.parse(path.basename(__filen
 for(var i = 0 ; i < (fs.readdirSync(path.join('input', path.parse(path.basename(__filename)).name)).filter(file => fs.statSync(path.join('input', path.parse(path.basename(__filename)).name, file)).isFile()).length); i++){
     let [outputFile , inputFilePath , outputFilePath] = [`output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`,`input/${path.parse(path.basename(__filename))["name"]}/input_${path.parse(path.basename(__filename))["name"]}_${i}.txt` , `output/${path.parse(path.basename(__filename))["name"]}/output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`];
 
-    //////////////////////////////////////// 2. Problem-dependent and language-dependent code  /////////////////////////////////////////
+    /********************************************* Problem-dependent and language-dependent code    /*********************************************/
 
-    const pythonArgs = ['-c', `
+let code = `
+print(str(eval(input_content.replace(' ','*')) // 2))
+    `;
+
+
+     /********************************************* Fixed code  /*********************************************/
+let pythonArgs = ['-c', `
 with open('${inputFilePath}', 'r') as infile:
     input_content = infile.read().strip()
-
-print(str(eval(input_content.replace(' ','*')) // 2))
-    `];
-
-    ///////////////////////////////////////// 3. Fixed code /////////////////////////////////////////////
+` + code];
     common(pythonArgs, outputFile, inputFilePath, outputFilePath);
 }
