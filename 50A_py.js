743d02b2848ad28604098437a4f362cc47da744f
Sreedhar K <sreedhar.k@rakuten.com>
2024-09-30 14:24:54 +0530

    C

--- 50A_py.js
+++ 50A_py.js
@@ -1,16 +1,9 @@
-/////////////////////////////////////// 1. Look for changes here  /////////////////////////////////////////
-const fs = require('fs');
-const path = require('path');
-const common = require('./fixed_code')
+let [fs , path,common] = [require('fs') , require('path'),require('./fixed_code')];
 
 console.log(`================================ ${path.parse(path.basename(__filename))["name"]} ================================`)
 
-
 for(var i = 0 ; i < (fs.readdirSync(path.join('input', path.parse(path.basename(__filename)).name)).filter(file => fs.statSync(path.join('input', path.parse(path.basename(__filename)).name, file)).isFile()).length); i++){
-
-    let outputFile = `output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`;
-    let inputFilePath = `input/${path.parse(path.basename(__filename))["name"]}/input_${path.parse(path.basename(__filename))["name"]}_${i}.txt`;
-    let outputFilePath = `output/${path.parse(path.basename(__filename))["name"]}/output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`;
+    let [outputFile , inputFilePath , outputFilePath] = [`output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`,`input/${path.parse(path.basename(__filename))["name"]}/input_${path.parse(path.basename(__filename))["name"]}_${i}.txt` , `output/${path.parse(path.basename(__filename))["name"]}/output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`];
 
     //////////////////////////////////////// 2. Problem-dependent and language-dependent code  /////////////////////////////////////////
 
