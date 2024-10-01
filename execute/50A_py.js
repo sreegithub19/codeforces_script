2ab3d065ec36d60f6546550e7b7913e8727c8dea
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-01 09:38:15 +0530

    C

--- execute/50A_py.js
+++ execute/50A_py.js
@@ -1,17 +1,8 @@
 /********************************************* Problem-dependent and language-dependent code    /*********************************************/
-
 let code = `
 print(str(eval(input_content.replace(' ','*')) // 2))
  `;
-
 /********************************************* Fixed code /*********************************************/
 
-let [fs , path,common] = [require('fs') , require('path'),require('../fixed_code')];
-console.log(`================================ ${path.parse(path.basename(__filename))["name"]} ================================`)
-
-for(var i = 0 ; i < (fs.readdirSync(path.join('./input', path.parse(path.basename(__filename)).name)).filter(file => fs.statSync(path.join('./input', path.parse(path.basename(__filename)).name, file)).isFile()).length); i++){
-    let [outputFile , inputFilePath , outputFilePath] = [`./output/output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`,`./input/${path.parse(path.basename(__filename))["name"]}/input_${path.parse(path.basename(__filename))["name"]}_${i}.txt` , `./output/${path.parse(path.basename(__filename))["name"]}/output_${path.parse(path.basename(__filename))["name"]}_${i}.txt`];
-    common(['-c', `
-with open('${inputFilePath}', 'r') as infile:
-    input_content = infile.read().strip()` + code], outputFile, inputFilePath, outputFilePath);
-}
+let [fs, path, common] = [require('fs'), require('path'), require('../fixed_code')], dirPath = path.join('./input', path.parse(path.basename(__filename)).name); 
+fs.readdirSync(dirPath).filter(file => fs.statSync(path.join(dirPath, file)).isFile()).forEach((_, i) => common(['-c', `with open('./input/${path.parse(path.basename(__filename)).name}/input_${path.parse(path.basename(__filename)).name}_${i}.txt', 'r') as infile: input_content = infile.read().strip()` + code], `./output/output_${path.parse(path.basename(__filename)).name}_${i}.txt`, `./input/${path.parse(path.basename(__filename)).name}/input_${path.parse(path.basename(__filename)).name}_${i}.txt`,  `./output/${path.parse(path.basename(__filename)).name}/output_${path.parse(path.basename(__filename)).name}_${i}.txt`));
