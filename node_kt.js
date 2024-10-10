be6c20ed336e94f95faa16adf8d5e8ab646c76f6
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-10 13:20:13 +0530

    C

---
+++ node_kt.js
@@ -0,0 +1,24 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+
+// Kotlin code as a string
+const kotlinCode = `
+fun greet(name: String): String {
+    return "Hello, \\$name!"
+}
+
+println(greet("World"))
+`;
+
+// Write the Kotlin code to a temporary file
+fs.writeFileSync('temp.kt', kotlinCode);
+
+// Compile the Kotlin code to JavaScript
+execSync('kotlinc temp.kt -include-runtime -d temp.jar');
+
+// Run the compiled Kotlin code
+execSync('java -jar temp.jar');
+
+// Clean up
+fs.unlinkSync('temp.kt');
+fs.unlinkSync('temp.jar');
