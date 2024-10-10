5ce73640870b45cadcf2d6b7f1544b4d1c8d8748
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-10 22:49:47 +0530

    C

--- execute/Utils.kt
+++ execute/Utils.kt
@@ -1,35 +1,30 @@
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 
 object Utils {
-    fun main(args: Array<String?>?) {
+    @JvmStatic
+    fun main(args: Array<String>) {
     }
 
     @Throws(IOException::class)
-    fun commonFunction(outputFilePath: String?, result: Int, i: Int) {
+    fun commonFunction(outputFilePath: String, result: Int, i: Int) {
         // Read contents from the output file
         val outputLines: List<String> = Files.readAllLines(Paths.get(outputFilePath))
         var found = false
 
         // Compare the computed result with each line in the output file
         for (line in outputLines) {
-            if (line.trim().equals(String.valueOf(result))) {
+            if (line.trim() == result.toString()) {
                 found = true
                 break
             }
         }
 
-        // String multilineString = """
-        //         This is a multiline string.
-        //         It can span multiple lines.
-        //         Each line is preserved as written.
-        //         """;
-
-        // System.out.println(multilineString);
-
         // Output result of the comparison
-        if (found) System.out.println("$i:true")
+        if (found) println("$i:true")
         else {
-            System.out.println("$i:false")
+            println("$i:false")
             System.exit(1)
         }
     }
