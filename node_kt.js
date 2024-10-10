17042afd4d69bd107f9d41b1443aaff312923956
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-10 13:26:56 +0530

    C

--- node_kt.js
+++ node_kt.js
@@ -4,16 +4,18 @@ const fs = require('fs');
 // Kotlin code as a string
 const kotlinCode = `
 fun greet(name: String): String {
-    return "Hello node, \\$name!"
+    return "Hello node, \$name!"
 }
 
-println(greet("World"))
+fun main() {
+    println(greet("World"))
+}
 `;
 
 // Write the Kotlin code to a temporary file
 fs.writeFileSync('temp.kt', kotlinCode);
 
-// Compile the Kotlin code to JavaScript
+// Compile the Kotlin code to a JAR file
 execSync('kotlinc temp.kt -include-runtime -d temp.jar');
 
 // Run the compiled Kotlin code
