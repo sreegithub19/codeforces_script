a919d25003efdf19c2949a4f2538d03bc2793398
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-12 14:42:07 +0530

    C

---
+++ execute/cpp_.rs
@@ -0,0 +1,26 @@
+use std::process::Command;
+
+fn main() {
+    let cpp_code = r#"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from C++!" << std::endl;
+    return 0;
+}
+"#;
+
+    // Compile and run the C++ code using a single command
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(format!(
+            "echo '{}' | g++ -x c++ -o hello - && ./hello",
+            cpp_code.replace("'", "'\"'\"'")
+        ))
+        .output()
+        .expect("Failed to execute command");
+
+    // Print the output of the C++ program
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    println!("{}", String::from_utf8_lossy(&output.stderr));
+}
