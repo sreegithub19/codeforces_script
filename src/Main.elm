63c2a5ac4c06f54627699dc4e9d726c6f2ab174f
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-15 17:06:35 +0530

    C

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text)
+import Html exposing (Html, text, div, h1, p)
 
 -- The main function to run the application
 main : Program () () ()
@@ -9,5 +9,13 @@ main =
     Browser.sandbox
         { init = ()
         , update = \_ model -> model
-        , view = \_ -> text "Hello, Elm World!"
+        , view = view
         }
+
+-- Define the view function to render HTML
+view : () -> Html msg
+view _ =
+    div []
+        [ h1 [] [ text "Hello, Elm World!" ]
+        , p [] [ text "This is a simple Elm application rendering HTML." ]
+        ]
