71ea4866d92e0e412ed897f01afecadb2c143043
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-25 15:26:19 +0530

    C

---
+++ execute/dart_in_python.py
@@ -0,0 +1,37 @@
+import subprocess
+
+def run_dart_code(dart_code):
+    # Prepare the Dart command with the eval option
+    command = f'dart --no-sound-null-safety -e "{dart_code}"'
+    
+    # Run the command
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    return result.stdout, result.stderr
+
+if __name__ == "__main__":
+    dart_code = '''
+import 'package:dart_eval/dart_eval.dart';
+
+void main() {
+    print(eval('2 + 2')); // -> 4
+    
+    final program = r\'\'\'
+        class Cat {
+        Cat(this.name);
+        final String name;
+        String speak() => "I'm $name!";
+        }
+        void main() {
+        final cat = Cat('Fluffy');
+        print(cat.speak()); // Print inside the program
+        }
+      \'\'\';
+      
+      eval(program, function: 'main'); // Calls main(), which prints 'I'm Fluffy!'
+}
+    '''
+
+    output, error = run_dart_code(dart_code)
+    print("Output:\n", output)
+    if error:
+        print("Error:\n", error)
