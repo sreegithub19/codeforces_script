c806c1bbeff9ee549b10b9c4a9c4ea5a9c770f3f
Sreedhar K <sreedhar.k@rakuten.com>
2024-10-31 14:25:29 +0530

    C

--- lua_/hello.lua
+++ lua_/hello.lua
@@ -14,3 +14,16 @@ handle:close()
 print("Output from Python:")
 print(result)
 
+-- Using io.popen with a multiline string
+local command = [[
+lua -e '
+print("Hello from inline Lua!")
+'
+]]
+
+local handle = io.popen(command)
+local result = handle:read("*a")
+handle:close()
+
+print("Output from Lua:")
+print(result)
