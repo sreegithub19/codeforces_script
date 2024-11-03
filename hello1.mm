a545f9cda10adfed9fb3a30ce4d7479ed8679ed9
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-03 22:02:09 +0530

    C

---
+++ hello1.mm
@@ -0,0 +1,26 @@
+#import <Foundation/Foundation.h>
+
+void findFoundationHeader() {
+    NSArray *searchPaths = @[
+        @"/usr/include/GNUstep/Foundation/Foundation.h",
+        @"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Headers/Foundation.h",
+        @"/usr/local/include/Foundation/Foundation.h",
+        @"/usr/include/Foundation/Foundation.h"
+    ];
+    
+    for (NSString *path in searchPaths) {
+        if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+            NSLog(@"Found Foundation.h at: %@", path);
+            return;
+        }
+    }
+    
+    NSLog(@"Foundation.h not found in the specified paths.");
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        findFoundationHeader();
+    }
+    return 0;
+}
