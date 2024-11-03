93bd5dd5cba92b1cbc4009ad74a2a830f295ff83
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-03 20:52:12 +0530

    C

---
+++ execute/objective_cpp_in_js.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+
+const objcCode = `
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello from Objective-C++! in multiline from Nodejs!");
+    }
+    return 0;
+}
+`;
+
+try {
+    // Compile and run the Objective-C++ code using a single command
+    const output = execSync(`echo '${objcCode.replace(/'/g, "'\\''")}' | clang++ -x objective-c++ -framework Foundation -o hello - && ./hello`, { stdio: 'pipe' });
+
+    // Print the output of the Objective-C++ program
+    console.log(output.toString());
+} catch (error) {
+    console.error(`Error: ${error.message}`);
+}
