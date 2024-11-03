31fac3a209d3266f22fd570ab9792fc875fdde38
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-03 20:03:50 +0530

    C

--- hello.mm
+++ hello.mm
@@ -2,13 +2,23 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        [[[[NSTask alloc] init] setExecutableURL:[NSURL fileURLWithPath:@"/bin/bash"]],
-          [[NSTask alloc] setArguments:@[@"-c", @"python3 -c \"print('Hello from Python in Objective C++!!')\""]],
-          [[NSTask alloc] setStandardOutput:[NSPipe pipe]],
-          [NSTask launchAndReturnError:nil],
-          [NSTask waitUntilExit]];
-        
-        NSLog(@"\n%@", [[NSString alloc] initWithData:[[[[NSTask standardOutput] fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding]]);
+        NSString *pythonCommand = @"print('Hello from Python in Objective C++!!')";
+        NSTask *task = [[NSTask alloc] init];
+        [task setExecutableURL:[NSURL fileURLWithPath:@"/bin/bash"]]; // Use bash for better compatibility
+        [task setArguments:@[@"-c", [NSString stringWithFormat:@"python3 -c \"%@\"", pythonCommand]]];
+        NSPipe *pipe = [NSPipe pipe];
+        [task setStandardOutput:pipe];
+        NSError *error = nil;
+        [task launchAndReturnError:&error];
+        if (error) {
+            NSLog(@"Error launching task: %@", error);
+            return 1;
+        }
+        [task waitUntilExit];
+        // NSData *data = [[pipe fileHandleForReading] readDataToEndOfFile];
+        // NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        // NSLog(@"\n%@", output);
+        NSLog(@"\n%@", [[NSString alloc] initWithData:[[pipe fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding]);
     }
     return 0;
 }
