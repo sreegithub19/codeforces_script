ff6f3f82414975fbc7ffeaedc50e051dba06f8ba
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-03 20:07:10 +0530

    C

--- hello.mm
+++ hello.mm
@@ -2,7 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *pythonCommand = @"print('Hello from Python in Objective C++!!')";
+        NSString *pythonCommand = R"(
+print('Hello from Python in Objective C++!!')
+print('This is a multiline string.')
+print('Using R for the task.')
+        )";
         NSTask *task = [[NSTask alloc] init];
         [task setExecutableURL:[NSURL fileURLWithPath:@"/bin/bash"]]; // Use bash for better compatibility
         [task setArguments:@[@"-c", [NSString stringWithFormat:@"python3 -c \"%@\"", pythonCommand]]];
@@ -15,9 +19,6 @@ int main(int argc, const char * argv[]) {
             return 1;
         }
         [task waitUntilExit];
-        // NSData *data = [[pipe fileHandleForReading] readDataToEndOfFile];
-        // NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        // NSLog(@"\n%@", output);
         NSLog(@"\n%@", [[NSString alloc] initWithData:[[pipe fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding]);
     }
     return 0;
