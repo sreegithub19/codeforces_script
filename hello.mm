de361287b1b32f73664f990633f167f0d933701f
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-03 20:18:18 +0530

    C

--- hello.mm
+++ hello.mm
@@ -1,15 +1,20 @@
 #import <Foundation/Foundation.h>
+#include <iostream>
+#include <string>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *pythonCommand = R"(
+        // Raw string literal should be std::string, not a pointer
+        std::string pythonCommand = R"(
 print('Hello from Python in Objective C++!!')
 print('This is a multiline string.')
 print('Using R for the task.')
-        )";
+)";
         NSTask *task = [[NSTask alloc] init];
         [task setExecutableURL:[NSURL fileURLWithPath:@"/bin/bash"]]; // Use bash for better compatibility
-        [task setArguments:@[@"-c", [NSString stringWithFormat:@"python3 -c \"%@\"", pythonCommand]]];
+        // Convert std::string to NSString
+        NSString *command = [NSString stringWithFormat:@"python3 -c \"%@\"", pythonCommand];
+        [task setArguments:@[@"-c", command]];
         NSPipe *pipe = [NSPipe pipe];
         [task setStandardOutput:pipe];
         NSError *error = nil;
@@ -19,7 +24,8 @@ print('Using R for the task.')
             return 1;
         }
         [task waitUntilExit];
-        NSLog(@"\n%@", [[NSString alloc] initWithData:[[pipe fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding]);
+        NSString *output = [[NSString alloc] initWithData:[[pipe fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding];
+        NSLog(@"\n%@", output);
     }
     return 0;
 }
