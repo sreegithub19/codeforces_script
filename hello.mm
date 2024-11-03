feec863e53b48e060dd7786480e8b4b66cca7d1f
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-03 19:59:01 +0530

    C

--- hello.mm
+++ hello.mm
@@ -2,19 +2,13 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSTask *task = [[NSTask alloc] init];
-        [task setExecutableURL:[NSURL fileURLWithPath:@"/bin/bash"]];
-        [task setArguments:@[@"-c", @"python3 -c \"print('Hello from Python in Objective C++!!')\""]];
-        [task setStandardOutput:[NSPipe pipe]];
-
-        NSError *error = nil;
-        if (![task launchAndReturnError:&error]) {
-            NSLog(@"Error launching task: %@", error);
-            return 1;
-        }
-
-        [task waitUntilExit];
-        NSLog(@"\n%@", [[[[task.standardOutput fileHandleForReading] readDataToEndOfFile] initWithData:encoding:NSUTF8StringEncoding]]);
+        [[[[NSTask alloc] init] setExecutableURL:[NSURL fileURLWithPath:@"/bin/bash"]],
+          [[NSTask alloc] setArguments:@[@"-c", @"python3 -c \"print('Hello from Python in Objective C++!!')\""]],
+          [[NSTask alloc] setStandardOutput:[NSPipe pipe]],
+          [NSTask launchAndReturnError:nil],
+          [NSTask waitUntilExit]];
+        
+        NSLog(@"\n%@", [[NSString alloc] initWithData:[[[[NSTask standardOutput] fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding]]);
     }
     return 0;
 }
