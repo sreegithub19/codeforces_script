8dcf84c272fd7bca9509ddf1336c59aecd183d06
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-04 13:29:53 +0530

    C

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.0;
 contract HelloWorld {
     string public message;
 
+    // Event declaration
+    event MessageUpdated(string newMessage);
+
     constructor() {
         message = "Hello, Solidity World!";
+        emit MessageUpdated(message); // Emit the event
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,21 @@
-const hre = require("hardhat");
-
 async function main() {
-    // Deploy the HelloWorld contract
-    const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
-    const helloWorld = await HelloWorld.deploy(); // Wait for deployment
-
-    await helloWorld.deployTransaction.wait(); // Wait for the transaction to be mined
+    // Get the contract factory
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    
+    // Deploy the contract
+    const helloWorld = await HelloWorld.deploy();
+    
+    // Wait for the deployment to finish
+    await helloWorld.deployed();
 
     console.log("HelloWorld deployed to:", helloWorld.address);
 
-    // Fetch and log the message
+    // Get the message from the contract
     const message = await helloWorld.message();
     console.log("Message from contract:", message);
 }
 
+// Execute the deployment script
 main()
     .then(() => process.exit(0))
     .catch((error) => {
