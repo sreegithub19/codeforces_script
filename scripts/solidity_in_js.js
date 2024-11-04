b9ed04d553d30e9459acc609c064869ce5578199
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-04 20:17:43 +0530

    C

---
+++ scripts/solidity_in_js.js
@@ -0,0 +1,67 @@
+const { ethers } = require("hardhat");
+const solc = require("solc");
+
+async function main() {
+    // Define the Solidity contract
+    const sourceCode = `
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.0;
+
+    contract HelloWorld {
+        string public message;
+
+        constructor() {
+            message = "Hello, solidity in js World!";
+        }
+    }`;
+
+    // Compile the contract
+    const input = {
+        language: 'Solidity',
+        sources: {
+            'HelloWorld.sol': {
+                content: sourceCode,
+            },
+        },
+        settings: {
+            outputSelection: {
+                '*': {
+                    '*': ['*'],
+                },
+            },
+        },
+    };
+
+    const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+    // Extract the compiled contract's bytecode and ABI
+    const contractData = output.contracts['HelloWorld.sol']['HelloWorld'];
+    const abi = contractData.abi;
+    const bytecode = contractData.evm.bytecode.object;
+
+    // Get a signer
+    const [deployer] = await ethers.getSigners();
+
+    // Get the contract factory using the ABI and bytecode
+    const HelloWorldFactory = new ethers.ContractFactory(abi, bytecode, deployer);
+
+    // Deploy the contract
+    const helloWorld = await HelloWorldFactory.deploy();
+
+    // Wait for the deployment to finish
+    //await helloWorld.deployed();
+
+    console.log("HelloWorld deployed to:", helloWorld.address);
+
+    // Get the message from the contract
+    const message = await helloWorld.message();
+    console.log("Message from contract:", message);
+}
+
+// Execute the deployment script
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
