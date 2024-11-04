5268e101b9cab38358a255e1744be21e301fab7c
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-04 12:57:41 +0530

    C

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat");
+
+async function main() {
+    // Deploy the HelloWorld contract
+    const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
+    const helloWorld = await HelloWorld.deploy();
+
+    await helloWorld.deployed();
+
+    console.log("HelloWorld deployed to:", helloWorld.address);
+
+    // Fetch and log the message
+    const message = await helloWorld.message();
+    console.log("Message from contract:", message);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
