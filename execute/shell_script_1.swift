cbd0968c052315a2f9d916a6c4770100766434d3
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-09 09:02:55 +0530

    C

--- execute/shell_script_1.swift
+++ execute/shell_script_1.swift
@@ -3,10 +3,21 @@ import Foundation
 // Create a Process instance
 let process = Process()
 
-// Set the arguments for the command to run
-process.arguments = ["/opt/hostedtoolcache/swift-Ubuntu/5.7.3/x64/usr/bin/swift","-e", """
+// Set the executable path (path to swift binary)
+process.executableURL = URL(fileURLWithPath: "/opt/hostedtoolcache/swift-Ubuntu/5.7.3/x64/usr/bin/swift")
+
+// Set the arguments for the command to run (multiline Swift code as a single string)
+let swiftCode = """
 print("Hello, Swift shell World from -e!")
-"""]
+let x = 42
+print("The answer is \(x)")
+"""
+
+// Escape the newline characters to make the entire code a single string
+let swiftArguments = swiftCode.replacingOccurrences(of: "\n", with: "\\n")
+
+// Pass the arguments (swift -e and the escaped Swift code)
+process.arguments = ["-e", swiftArguments]
 
 // Create a Pipe to capture the output
 let pipe = Pipe()
@@ -15,7 +26,7 @@ process.standardOutput = pipe
 do {
     // Launch the process
     try process.run()
-    
+
     // Read the output
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     if let output = String(data: data, encoding: .utf8) {
