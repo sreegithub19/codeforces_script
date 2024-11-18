1e2142a0c85354134152dfd34ad229e248ecb68d
Sreedhar K <sreedhar.k@rakuten.com>
2024-11-18 13:45:08 +0530

    C

--- runWasi.js
+++ runWasi.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const { Wasmer } = require('@wasmer/wasi');
+const { WASI } = require('@wasmer/wasi');
+const { argv, env } = require('process');
 
 async function runWasi() {
-  const wasi = new Wasmer();
+  const wasi = new WASI({
+    args: argv,
+    env,
+    preopens: {
+      '/sandbox': './'
+    }
+  });
 
   // Load and instantiate the WASM module
-  const wasmPath = path.resolve(__dirname, 'build/greet.wasm');
+  const wasmPath = path.join(__dirname, 'build/greet.wasm');
   const wasmBytes = fs.readFileSync(wasmPath);
 
   const { instance } = await wasi.instantiate(wasmBytes);
