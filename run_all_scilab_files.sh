b3ecb597f6abe9cd6bb9447e836414c23c69ba23
Sreedhar K <sreedhar.k@rakuten.com>
2024-12-15 22:08:20 +0530

    C

--- run_all_scilab_files.sh
+++ run_all_scilab_files.sh
@@ -4,7 +4,7 @@
 SCILAB_DIR="files"
 
 # Loop through all .m files in the specified directory
-for scilab_file in "$SCILAB_DIR"/*.m
+for scilab_file in "$SCILAB_DIR"/*.sci
 do
   if [[ -f "$scilab_file" ]]; then
     echo "Running $scilab_file..."
