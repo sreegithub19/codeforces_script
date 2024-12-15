9561bf1509c1bca998b0716b681911cbdf09038d
Sreedhar K <sreedhar.k@rakuten.com>
2024-12-15 21:12:56 +0530

    C

--- run_all_scilab_files.sh
+++ run_all_scilab_files.sh
@@ -9,6 +9,6 @@ do
   if [[ -f "$scilab_file" ]]; then
     echo "Running $scilab_file..."
     # Run each file using SCILAB -batch
-    scilab -no_gui -batch "exec('$scilab_file')"
+    scilab -nogui -batch "exec('$scilab_file')"
   fi
 done
