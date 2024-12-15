53fe676b0971d000a46f9f47aa9c13c108c44c36
Sreedhar K <sreedhar.k@rakuten.com>
2024-12-15 20:03:18 +0530

    C

---
+++ files/hello.sci
@@ -0,0 +1,2 @@
+// hello_world.sci
+disp("Hello,File scilab World!");
---
+++ run_all_scilab_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Change this directory to the folder containing your SCILAB scripts
+SCILAB_DIR="files"
+
+# Loop through all .m files in the specified directory
+for scilab_file in "$SCILAB_DIR"/*.m
+do
+  if [[ -f "$scilab_file" ]]; then
+    echo "Running $scilab_file..."
+    # Run each file using SCILAB -batch
+    scilab -batch "run('$scilab_file')"
+  fi
+done
