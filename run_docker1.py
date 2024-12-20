4d343bfb153b125ae99fabbd8f84646d0780b7dd
Sreedhar K <sreedhar.k@rakuten.com>
2024-12-20 14:57:08 +0530

    C

--- run_docker1.py
+++ run_docker1.py
@@ -9,6 +9,7 @@ def run_docker():
             docker run -d --name my-container --mount type=tmpfs,dst=/tmp/my_tmpfs my-docker-image sleep 3600 && \
             docker exec my-container bash -c 'echo "<html><body><h1>Hello from Docker here in memory!</h1></body></html>" > /tmp/my_tmpfs/index1_container.html' && \
             sleep 10 && \
+            docker exec -it my-container bash && ls -l /tmp/my_tmpfs/ && \
             docker cp my-container:/tmp/my_tmpfs/index1_container.html ./index1.html && \
             docker stop my-container && \
             docker rm my-container
