d1e02d2025e89663b70dbafd337461f8062df5e6
Sreedhar K <sreedhar.k@rakuten.com>
2024-12-24 07:40:03 +0530

    C

---
+++ _5_input.rs
@@ -0,0 +1,26 @@
+fn main() {
+    use std::io::{stdin,stdout,Write};
+    let mut s=String::new();
+    print!("Please enter some text: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    println!("You typed: {}",s);
+
+
+    print!("Please enter some text again: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    println!("You againtyped: {}",s);
+}
