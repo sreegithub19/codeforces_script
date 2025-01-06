d2dc666322fba249be68fc67ae8ac9c6bc14f825
Sreedhar K <sreedhar.k@rakuten.com>
2025-01-06 12:52:08 +0530

    C

---
+++ shell_script.py
@@ -0,0 +1,14 @@
+exec('''
+sudo apt-get install -y maven
+mvn dependency:copy-dependencies -DoutputDirectory=libs
+python -m pip install --upgrade pip
+pip install jupyter nbconvert
+python test.py
+cd execute
+jupyter nbconvert --to notebook --execute notebook.ipynb --output executed_notebook.ipynb
+cd .. 
+cd execute
+jupyter nbconvert --to html executed_notebook.ipynb --output executed_notebook
+cd .. 
+ls -la
+''')
